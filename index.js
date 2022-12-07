60e48cd367282baa2478f3e6088276af23cb25d2
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-12-07 16:51:36 +0100

    finished middleware practice

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-app.use(morgan('dev'));
-app.use((req, res, next) => {
-  req.requestTime = Date.now();
-  req.method = 'GET'; // change the method to GET for every request
+// app.use(morgan('dev'));
+// app.use((req, res, next) => {
+//   req.requestTime = Date.now();
+//   req.method = 'GET'; // change the method to GET for every request
+//   next();
+// });
+
+app.use('/dogs', (req, res, next) => {
+  console.log('I love dogs!');
   next();
+});
+
+// Really simple authentication
+const verifyPassword = (req, res, next) => {
+  const { password } = req.query;
+  if (password === 'chickennugget') {
+    next();
+  } else {
+    res.send('Sorry you need a password!');
+  }
+};
+
+app.get('/secret', verifyPassword, (req, res) => {
+  res.send('My secret');
 });
 
 // app.use(morgan('dev'));
@@ -28,6 +47,10 @@ app.get('/dog', (req, res) => {
   console.log(`Request Date: ${req.requestTime}`);
 
   res.send('Woof!');
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
 });
 
 app.listen(3000, () => {
