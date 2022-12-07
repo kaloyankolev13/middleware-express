a7021febbcde0f02290d893ea3ddca263936cfbc
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-12-07 16:27:16 +0100

    practice on middleware

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-morgan('tiny');
-app.use(morgan('tiny'));
-app.use((req, res) => {
-  res.send('Hello world');
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  req.requestTime = Date.now();
+  req.method = 'GET'; // change the method to GET for every request
+  next();
 });
 
+// app.use(morgan('dev'));
+// app.use((req, res, next) => {
+//   console.log('Hello');
+//   return next(); // return next() will stop the execution of the function
+// });
+// app.use((req, res, next) => {
+//   console.log('Hello 2');
+//   next();
+// });
+
 app.get('/', (req, res) => {
+  console.log(`Request Date: ${req.requestTime}`);
   res.send('Hello World!');
 });
 
 app.get('/dog', (req, res) => {
+  console.log(`Request Date: ${req.requestTime}`);
+
   res.send('Woof!');
 });
 
